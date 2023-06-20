ae68ac64cc9633545939cb03e1dd3c9cf18064aa
Antoine Augusti <antoine.augusti@gmail.com>
2023-06-20 18:48:44 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Données de disponibilité de Divia Vélodi
+
+Collecte et archive automatiquement des données de disponibilité des parkings en ouvrage DiviaPark de la ville de Dijon.
+
+## Licences
+- [MIT](https://spdx.org/licenses/MIT.html) pour le code source
+- [Licence ouverte](https://spdx.org/licenses/etalab-2.0.html) pour les données
---
+++ main.py
@@ -0,0 +1,42 @@
+import csv
+import os
+import datetime
+import requests
+
+API = "https://www.data.gouv.fr/api/1"
+API_KEY = os.getenv("API_KEY")
+DATASET = "6491d5d41882972a92567d36"
+RESOURCE = "3c718b51-b629-43f4-aac3-93df951d1281"
+HEADERS = {"X-API-KEY": API_KEY}
+
+
+def api_url(path):
+    return "".join([API, path])
+
+
+url = "https://data.metropole-dijon.fr/api/records/1.0/search/?dataset=dispo-parking&q=&sort=nom_parking&facet=nom_parking"
+
+response = requests.get(url)
+response.raise_for_status()
+now = datetime.datetime.now().replace(microsecond=0).isoformat()
+data = []
+for record in response.json()["records"]:
+    fields = record["fields"]
+    data.append([now, fields["nom_parking"], round(fields["taux_doccupation"])])
+
+with requests.get(
+    f"https://www.data.gouv.fr/fr/datasets/r/{RESOURCE}", stream=True
+) as r:
+    r.raise_for_status()
+    with open("data.csv", "wb") as f:
+        for chunk in r.iter_content(chunk_size=8192):
+            f.write(chunk)
+
+with open("data.csv", "a") as f:
+    csv.writer(f).writerows(data)
+
+requests.post(
+    api_url(f"/datasets/{DATASET}/resources/{RESOURCE}/upload/"),
+    files={"file": open("data.csv", "rb")},
+    headers=HEADERS,
+).raise_for_status()
